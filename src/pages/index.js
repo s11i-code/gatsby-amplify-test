99303a3662c87d960b10ea35c4735e3290f7c21c
Satu <satu.salekari@sc5.io>
2019-08-08 17:38:56 +0300

    Changes 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 import React from "react"
 
-export default () => <div>Hello from Satu's test Gatsby app! After changes.</div>
+export default () => <div>Hello from Satu's test Gatsby app! After changes 2.</div>
